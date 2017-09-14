fea7a9cc12b16dfafa249d9d72368b29fe016e61
rmatheney7 <rmatheney7@gmail.com>
2017-09-14 15:16:32 -0500

    routere routed

---
+++ src/components/test.vue
@@ -0,0 +1,26 @@
+<template>
+    <div id="test">
+        <p>;f;ifiugyf</p>
+       
+    </div>
+</template>
+
+<script>
+// Imports
+export default {
+    data () {
+        return {
+            data: {
+                name: 'test Page'
+            }
+            
+            
+        }
+    },
+    
+}
+</script>
+
+<style scoped>
+
+</style>
