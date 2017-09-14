df0b85fe0275f53fc50838b3e36e4551c4a68eac
rmatheney7 <rmatheney7@gmail.com>
2017-09-14 15:19:12 -0500

    manually commited route changes

---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import test from './components/test.vue';
+//import showBlogs from './components/showBlogs.vue';
+
+export default [
+    //{ path: '/', component: App},
+    //{ path: '/test', component: newComp}
+    //{ path: '/add', component: addBlog}
+]
