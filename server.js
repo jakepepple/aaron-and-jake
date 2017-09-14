ff95825efb10e0a9fd31d87dd3e4a53d015e43a6
Jake Pepple <jakepeppledrums@gmail.com>
2017-09-14 15:22:54 -0500

    Server init

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const expressVue = require('express-vue');
+const path = require('path');
 require('dotenv').config();
+
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('hello world');
+const vueOptions = {
+  rootPath: path.join(__dirname, './src'),
+  
+};
+
+const expressVueMiddleWare = expressVue.init(vueOptions);
+app.use(expressVueMiddleWare);
+
+app.get('/', (req, res, next) => {
+  res.render('App');
 });
+
+
 
 const port = process.env.PORT;
 
