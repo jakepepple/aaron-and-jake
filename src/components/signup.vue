f256b4ea03aa1bcc33be5a98dbe3048fb7476db4
rmatheney7 <rmatheney7@gmail.com>
2017-09-18 16:21:18 -0500

    post requests on login and signup

--- src/components/signup.vue
+++ src/components/signup.vue
@@ -1,26 +1,64 @@
 <template>
     <div id="Sign Up">
         <p>Sign Up Page</p>
-       
+        <form>
+            <input type="text" v-model="signUpForm.email"  placeholder="E-mail Address" required/>
+            <input type="text" v-model="signUpForm.name"  placeholder="Username" required/>
+            <input type="password" v-model="signUpForm.password"  placeholder="Password" required/>
+            <input type="text" v-model="signUpForm.city"  placeholder="Current City" required/>
+            <button @click.prevent="signUp">Sign Up</button>
+        </form>
+
     </div>
 </template>
 
 <script>
 // Imports
+
 export default {
-    data () {
+    data() {
         return {
-            data: {
-                name: 'test Page'
-            }
-            
-            
+            signUpForm: {
+                name: '',
+                password: '',
+                city: '',
+                email: ''
+
+            },
+
+
         }
     },
-    
+    methods: {
+        signUp() {
+            this.$http.post('http://185548d4.ngrok.io/signup', {
+                name: this.signUpForm.name,
+                password: this.signUpForm.password,
+                city: this.signUpForm.city,
+                email: this.signUpForm.email
+            }).then(function(data, status, request) {
+                console.log(data);
+                console.log(status);
+                console.log(request);
+                 this.signUpForm.name = ''
+                this.signUpForm.password = ''
+                this.signUpForm.city = ''
+                this.signUpForm.email = ''
+            })
+
+
+        }
+
+    }
+
 }
 </script>
 
-<style scoped>
 
+<style scoped>
+.body {
+    background: url(../assets/Walkthrough_03@3x.png);
+    background-repeat: no-repeat;
+    background-size: cover;
+}
 </style>
