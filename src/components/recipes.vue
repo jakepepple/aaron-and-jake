725e15075cfcd2353947b766db9fdd4aa311acc7
rmatheney7 <rmatheney7@gmail.com>
2017-09-20 01:30:48 -0500

    List of recipes populating with v-for, recipes list reformatted to new component

---
+++ src/components/recipes.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id='recipes'>
+        <ul>
+            <li v-for="meal in meals">
+                <img v-bind:src="meal.recipe.image"><br/> {{meal.recipe.label}}
+                <br/>
+                <ul>
+                    <li v-for="Ingredient in meal.recipe.ingredientLines">
+                        {{Ingredient}}
+                    </li>
+                </ul>
+                {{meal.recipe.shareAs}}<br/>
+
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+// Imports
+export default {
+    props: ['meals'],
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style >
+
+</style>
