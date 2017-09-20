8de30a8a7c6278cd3ab17e851425b06fe92f01cf
Jake Pepple <jakepeppledrums@gmail.com>
2017-09-19 21:31:40 -0500

    Refactor db and server files, add event table to db

--- server.js
+++ server.js
@@ -1,108 +1,108 @@
 require('dotenv').config();
 const path = require('path');
-//express requirements
+// express requirements
 const express = require('express');
 const expressVue = require('express-vue');
 const session = require('express-session');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-//webpack requirements
+// webpack requirements
 const webpack = require('webpack');
 const webpackConfig = require('./webpack.config.js');
 const webpackMiddleware = require('webpack-dev-middleware');
+
 const compiler = webpack(webpackConfig);
-//database requirements
-const db = require('./db-config');
+// database requirements
 const passwordHash = require('password-hash');
-const User = db.User;
-//passport requirements
+const db = require('./db-config');
+const { User, Event } = { db };
+// passport requirements
 const passport = require('passport');
 const JsonStrategy = require('passport-json').Strategy;
+// google maps requirement
+const googleMapsClient = require('@google/maps').createClient({
+  key: process.env.GOOGLE_MAPS_API_KEY,
+});
 
 
 const app = express();
 
-//webpack middleware
+// webpack middleware
 app.use(webpackMiddleware(compiler, {
-  publicPath: webpackConfig.output.publicPath
-}))
+  publicPath: webpackConfig.output.publicPath,
+}));
 
-//vue middleware
+// vue middleware
 const vueOptions = {
-  rootPath: path.join(__dirname, '/dist/'), 
+  rootPath: path.join(__dirname, '/dist/'),
 };
 const expressVueMiddleWare = expressVue.init(vueOptions);
 app.use(expressVueMiddleWare);
 app.use(express.static(path.join(__dirname, '/dist/')));
 
-//cross domain access
-var allowCrossDomain = (req, res, next) => {
+// cross domain access
+const allowCrossDomain = (req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'POST, GET');
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
-}
+};
 app.use(allowCrossDomain);
 
 
-//passport strategy set-up
+// passport strategy set-up
 passport.use(new JsonStrategy({
-    usernameProp: 'email',
-    passwordProp: 'password',
-    passReqToCallback: true
-  }, (req, username, password, done) => {
-    User.findOne({ where: { email: username }}).then((user) => {
-      console.log('authenticating' );    
-      if (!user) {
-        console.log('no user');
-        return done(null, false, { message: 'No user with that email in directory.' });
-      }
-      if (!passwordHash.verify(password, user.dataValues.Password)) {
-        console.log(password, user);
-        console.log('wrong password');
-        return done(null, false, { message: 'Incorrect password!' });
-      }
-      console.log('about to serialize');
-      passport.serializeUser((user, done) => {
-        
-        return done(null, user.id);
-      })
-      passport.deserializeUser((id, done) => {
-        User.findById(id).then((user) => {
-          if (user) {
-            done(null, user.get());
-          } else {
-            done(user.errors, null);
-          }
-        })
-      })
-      return done(null, user);
-
-    })
+  usernameProp: 'email',
+  passwordProp: 'password',
+  passReqToCallback: true,
+}, (req, username, password, done) => {
+  User.findOne({ where: { email: username } }).then((user) => {
+    console.log('authenticating');
+    if (!user) {
+      console.log('no user');
+      return done(null, false, { message: 'No user with that email in directory.' });
+    }
+    if (!passwordHash.verify(password, user.dataValues.Password)) {
+      console.log(password, user);
+      console.log('wrong password');
+      return done(null, false, { message: 'Incorrect password!' });
+    }
+    console.log('about to serialize');
+    passport.serializeUser((user, done) => done(null, user.id));
+    passport.deserializeUser((id, done) => {
+      User.findById(id).then((user) => {
+        if (user) {
+          done(null, user.get());
+        } else {
+          done(user.errors, null);
+        }
+      });
+    });
+    return done(null, user);
+  })
     .catch((err) => {
       console.log('error logging in:', err);
-      return done(err)
-    })
-  }
-))
+      return done(err);
+    });
+}));
 
-//passport middleware
-app.use(session({ secret: 'dinnertime'}));
-app.use(bodyParser.json({extended: false}));
+// passport middleware
+app.use(session({ secret: 'dinnertime' }));
+app.use(bodyParser.json({ extended: false }));
 app.use(passport.initialize());
 app.use(passport.session());
 
 
-//Routes:
+// Routes:
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'))
-})
+  console.log('sent with app.get');
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 
 
-
-app.get('/browse', (req, res, next) => {
-  //IN PROGRESS
+app.get('/browse', (req, res) => {
+  // IN PROGRESS
   // console.log('connected');
   // const data = {
   //   otherData: 'something from the server'
@@ -111,20 +111,19 @@ app.get('/browse', (req, res, next) => {
   // next();
 });
 
-app.post('/login', passport.authenticate('json', {failureRedirect: '/login'}), (req, res) => {
-  //IN PROGRESS
-  
-  console.log('logging in success')
-  console.log(res)
+app.post('/login', passport.authenticate('json', { failureRedirect: '/login' }), (req, res) => {
+  // IN PROGRESS
+
+  console.log('logging in success');
   res.status(201).send(`Successfully logged in as: ${req.body.email}`);
   // passport.authenticate('json', {
   //   successRedirect: '/profile',
   //   failureRedirect: '/login',
-  //   failureFlash: 'Incorrect email or password' 
+  //   failureFlash: 'Incorrect email or password'
   // })
 });
 
-app.get('/profile', (req, res, next) => {
+app.get('/profile', (req, res) => {
   if (session.user) {
     res.status(200).send('Logged in: Profile access granted');
   } else {
@@ -132,20 +131,21 @@ app.get('/profile', (req, res, next) => {
   }
 });
 
-app.post('/signup', (req, res, next) => {
+app.post('/signup', (req, res) => {
   console.log(req);
   console.log(req.body);
-  let hash = passwordHash.generate(req.body.password);
-  //TEST password-hash
-  
+  const hash = passwordHash.generate(req.body.password);
+  // TEST password-hash
+
   User.findOrCreate({
-    where: { Email: req.body.email }, defaults: {
+    where: { Email: req.body.email },
+    defaults: {
       Name: req.body.name,
       Host_Rating: 0,
       Contributor_Rating: 0,
       City: req.body.city,
-      Password: hash
-    }
+      Password: hash,
+    },
   })
     .spread((user, created) => {
       console.log(user.get({ plain: true }));
@@ -153,15 +153,44 @@ app.post('/signup', (req, res, next) => {
       if (created) {
         res.status(201).send('User successfully created');
       } else {
-        res.status(200).send(user.get({plain: true}));
+        res.status(200).send(user.get({ plain: true }));
       }
-    })
+    });
 });
 
-app.post('/create', (req, res, next) => {
-
+app.post('/create', ({ body }, res) => {
+  const { location, name, recipeID, time } = { body };
+  // Calculate Latitude and Longitude from this address
+  googleMapsClient.geocode({
+    address: location,
+  }, (err, response) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(response);
+    }
+  });
+  // Make sure to put this in a 'then' function:
+  const latitude = location;
+  const longitude = location;
+  Event.findOrCreate({
+    where: { Name: name },
+    defaults: {
+      RecipeID: recipeID,
+      LocationLat: latitude,
+      LocationLng: longitude,
+      Time: time,
+      Host: session.user,
+    },
+  })
+    .spread((event, created) => {
+      if (created) {
+        res.status(201).send('Event successfully created');
+      } else {
+        res.status(200).send('Event already exists! Pick another name.');
+      }
+    });
 });
-
 
 
 const port = process.env.PORT;
@@ -170,7 +199,7 @@ var server = app.listen(port, () => {
   console.log(`app listening on port ${port}`);
 });
 
-//TEST SERVER
+// TEST SERVER
 // app.listen(80, 'localhost', function() {
 //   console.log('successfully hosting on 3001');
 // })
