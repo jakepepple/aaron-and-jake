789cf399188ec244f9e9674a57259f7995732193
rmatheney7 <rmatheney7@gmail.com>
2017-09-20 00:36:30 -0500

    Reformatting App View, Navbar and footer are independent templates with scoped styles

---
+++ src/components/footer.vue
@@ -0,0 +1,29 @@
+<template>
+    <footer>
+        {{copyright}}
+    </footer>
+</template>
+
+<script>
+// Imports
+export default {
+    data() {
+        return {
+            copyright: 'Copyright 2017 Buckeyed Seminole'
+           
+        }    
+    },
+    methods: {
+       
+    }
+}
+</script>
+
+<style scoped>
+footer{
+    background-color: #333;
+    overflow: hidden;
+
+}
+
+</style>
---
+++ src/components/navBar.vue
@@ -0,0 +1,57 @@
+<template>
+    <div>
+        <div class="topnav" id="myTopnav">
+            <router-link to="/home">Home</router-link>
+            <router-link to="/profile">Profile</router-link>
+            <router-link to="/login">Login/signup</router-link>
+            <router-link to="/browse">Browse</router-link>
+            <router-link to="/create">Create</router-link>
+        </div>
+
+    </div>
+</template>
+
+<script>
+// Imports
+export default {
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+.topnav {
+    background-color: #333;
+    overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+    float: left;
+    display: block;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+/* Add a color to the active/current link */
+.topnav a.active {
+    background-color: #4CAF50;
+    color: white;
+}
+
+</style>
