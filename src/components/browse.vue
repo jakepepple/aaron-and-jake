9ad0bffed8c124f069adce1d5147fa5a13b6646d
rmatheney7 <rmatheney7@gmail.com>
2017-09-20 22:11:46 -0500

    map and markers populating

--- src/components/browse.vue
+++ src/components/browse.vue
@@ -1,45 +1,79 @@
 <template>
-    <div id="Browse">
-        <gmap-map
-    :center="center"
-    :zoom="7"
-    style="width: 500px; height: 300px"
-  >
-    
-  </gmap-map>
-       
+    <div class="google-map" :id="mapName">
     </div>
 </template>
-
 <script>
-// Imports
-import maps from '../../node_modules/vue2-google-maps/dist/components/map.vue'
-import marker from '../../node_modules/vue2-google-maps/dist/components/map.vue'
-
-
 export default {
-    components:{
-        'gmapMap':maps,
-        'gmapMarker': marker  
-
-
-    },
-    data () {
+    name: 'google-map',
+    props: ['name'],
+    data: function() {
         return {
-            center: {lat: 10.0, lng: 10.0},
-        markers: [{
-          position: {lat: 10.0, lng: 10.0}
-        }, {
-          position: {lat: 11.0, lng: 11.0}
-        }]
-            
-            
+            mapName: this.name + "-map",
+            markerCoordinates: [{
+                latitude: 29.9511,
+                longitude: -90.0715
+            }],
+            map: null,
+            bounds: null,
+            markers: [],
         }
     },
-    
-}
+
+    mounted: function() {
+        this.bounds = new google.maps.LatLngBounds();
+        const element = document.getElementById(this.mapName)
+        const mapCentre = this.markerCoordinates[0]
+        const options = {
+            center: new google.maps.LatLng(mapCentre.latitude, mapCentre.longitude)
+        }
+        this.map = new google.maps.Map(element, options);
+
+        this.$http.get('http://61e83bf3.ngrok.io/browse')
+            .then(function(response) {
+                let arr = []
+                response.body.forEach(function(element) {
+                    let tempLat = element.LocationLat;
+                    let tempLong = element.LocationLng;
+                    console.log(element, tempLat, tempLong)
+                    arr.push({ latitude: tempLat, longitude: tempLong })
+                    console.log({ latitude: tempLat, longitude: tempLong })
+                });
+                arr.forEach((coord) => {
+                    const position = new google.maps.LatLng(coord.latitude, coord.longitude);
+                    const marker = new google.maps.Marker({
+                        position,
+                        map: this.map
+                    });
+                    this.markers.push(marker)
+                    this.map.fitBounds(this.bounds.extend(position))
+
+                });
+
+
+            })
+
+
+
+
+        this.markerCoordinates.forEach((coord) => {
+            const position = new google.maps.LatLng(coord.latitude, coord.longitude);
+            const marker = new google.maps.Marker({
+                position,
+                map: this.map
+            });
+            this.markers.push(marker)
+            this.map.fitBounds(this.bounds.extend(position))
+        });
+
+
+    }
+};
 </script>
-
-<style>
-
+<style scoped>
+.google-map {
+    width: 800px;
+    height: 600px;
+    margin: 0 auto;
+    background: gray;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,17 +3,11 @@ import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import App from './App.vue';
 import Routes from './routes';
-import * as VueGoogleMaps from 'vue2-google-maps';
 
 // Use packages
 
 Vue.use(VueResource);
-Vue.use(VueGoogleMaps, {
-  load: {
-    key: 'AIzaSyC_L-4LysDxyPUtRJftl4aPOMc9Xkkjsb0',
 
-  },
-});
 Vue.use(VueRouter);
 
 
