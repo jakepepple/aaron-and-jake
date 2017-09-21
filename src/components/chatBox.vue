27d4a66d45c41725340db485bd90b48df6a3d2c5
rmatheney7 <rmatheney7@gmail.com>
2017-09-21 14:14:35 -0500

    Fixed chat

--- src/components/chatBox.vue
+++ src/components/chatBox.vue
@@ -9,18 +9,48 @@
         <button id="send">Send</button>
     </div>
 </template>
-<script src="../socket.js"></script>
+
 
 <script>
 // Imports
 export default {
     data() {
         return {
-           
-        }    
+
+        }
     },
-    methods: {
-       
+    mounted() {
+        const socket = io.connect('http://61e83bf3.ngrok.io');
+
+
+        let message = document.getElementById('message');
+        let handle = document.getElementById('handle');
+        let btn = document.getElementById('send');
+        let output = document.getElementById('output');
+        let feedback = document.getElementById('feedback');
+
+
+        btn.addEventListener('click', () => {
+            socket.emit('chat', {
+                message: message.value,
+                handle: handle.value,
+            });
+        });
+
+        message.addEventListener('keypress', () => {
+            socket.emit('typing', handle.value);
+        });
+
+
+        socket.on('chat', (data) => {
+            feedback.innerHTML = '';
+            output.innerHTML += `<p><strong>${data.handle}:</strong>${data.message}</p>`;
+        });
+
+
+        socket.on('typing', (data) => {
+            feedback.innerHTML = '<p><em>' + data + ' is typing a message...</em></p>'
+        });
     }
 }
 
@@ -31,50 +61,50 @@ export default {
 </script>
 
 <style scoped>
-h2{
+h2 {
     font-size: 18px;
     padding: 10px 20px;
     color: #575ed8;
 }
 
-#chat-box{
+#chat-box {
     max-width: 600px;
     margin: 30px auto;
     border: 1px solid #ddd;
-    box-shadow: 1px 3px 5px rgba(0,0,0,0.05);
+    box-shadow: 1px 3px 5px rgba(0, 0, 0, 0.05);
     border-radius: 2px;
 }
 
-#chat-window{
+#chat-window {
     height: 400px;
     overflow: auto;
     background: #f9f9f9;
 }
 
-#output p{
+#output p {
     padding: 14px 0px;
     margin: 0 20px;
     border-bottom: 1px solid #e9e9e9;
     color: #555;
 }
 
-#feedback p{
+#feedback p {
     color: #aaa;
     padding: 14px 0px;
     margin: 0 20px;
 }
 
-#output strong{
+#output strong {
     color: #575ed8;
 }
 
-label{
+label {
     box-sizing: border-box;
     display: block;
     padding: 10px 20px;
 }
 
-input{
+input {
     padding: 10px 20px;
     box-sizing: border-box;
     background: #eee;
@@ -87,7 +117,7 @@ input{
     font-size: 16px;
 }
 
-button{
+button {
     background: #575ed8;
     color: #fff;
     font-size: 18px;
@@ -96,7 +126,4 @@ button{
     width: 100%;
     border-radius: 0 0 2px 2px;
 }
-
-
-
 </style>
