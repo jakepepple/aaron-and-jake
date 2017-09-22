ff03bc0b2e55f603c10536764c1c60d9a59d00ac
rmatheney7 <rmatheney7@gmail.com>
2017-09-22 16:02:37 -0500

    In event map

--- src/components/event.vue
+++ src/components/event.vue
@@ -1,22 +1,22 @@
 <template>
     <div id="Event">
-         <div id="app">
-  <button id="show-modal" @click="showModal = true">Show Modal</button>
-  <!-- use the modal component, pass in the prop -->
-  <chat v-if="showModal" @close="showModal = false">
-    <!--
-      you can use custom content here to overwrite
-      default content
-    -->
-    <h3 slot="header">custom header</h3>
-  </chat>
-</div>
+        <div id="app">
+            <button id="show-modal" @click="showModal = true">Show Modal</button>
+            <chat v-if="showModal" v-bind:event='event' @close="showModal = false">
+              
+                <h3 slot="header">{{event.Name}}</h3>
+            </chat>
+        </div>
         <p>Host: {{event.Host}}</p>
-        <p>Party Name: {{event.name}}</p>
+        <p>Party Name: {{event.Name}}</p>
         <p>Reciepe: {{event.RecipeID}}</p>
         <p>Lat: {{event.LocationLat}}</p>
         <p>Lng: {{event.LocationLng}}</p>
         <p>Time: {{event.Time}}</p>
+        <template>
+            <div class="google-map" :id="mapName">
+            </div>
+</template>
 
        
     </div>
@@ -26,24 +26,64 @@
 // Imports
 import chat from './chatBox.vue';
 export default {
-     components: {
+    components: {
         chat: chat,
     },
+    name: 'google-map',
     props: ['event'],
-    data () {
+    data() {
         return {
+            mapName: this.name + "-map",
+            markerCoordinates: [{
+                latitude: this.event.LocationLat,
+                longitude: this.event.LocationLng,
+            }],
+            map: null,
+            bounds: null,
+            markers: [],
             showModal: false,
             data: {
                 name: 'test Page'
             }
-            
-            
+
+
         }
     },
-    
+    mounted: function() {
+        this.bounds = new google.maps.LatLngBounds();
+        const element = document.getElementById(this.mapName)
+        const mapCentre = this.markerCoordinates[0]
+        const options = {
+            center: new google.maps.LatLng(mapCentre.latitude, mapCentre.longitude),
+            zoom: -50,
+        }
+        this.map = new google.maps.Map(element, options);
+
+
+
+
+        this.markerCoordinates.forEach((coord) => {
+            const position = new google.maps.LatLng(coord.latitude, coord.longitude);
+            const marker = new google.maps.Marker({
+                position,
+                map: this.map
+            });
+            this.markers.push(marker)
+            this.map.fitBounds(this.bounds.extend(position))
+        });
+
+
+    }
+
 }
 </script>
 
 <style scoped>
+.google-map {
+    width: 300px;
+    height: 300px;
+    margin: 0 auto;
+    background: gray;
+}
 
 </style>
