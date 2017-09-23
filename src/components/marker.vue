09179d8585f2f1d6706224e7c3d5222e7c3386b2
rmatheney7 <rmatheney7@gmail.com>
2017-09-23 15:50:43 -0500

    map fix

---
+++ src/components/marker.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div id="content">
+            <div id="siteNotice">
+            </div>
+            <h1 id="firstHeading" class="firstHeading">Hello</h1>
+            <div id="bodyContent">
+                <p>
+
+                </p>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script>
+// Imports
+export default {
+    props: ['event'],
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
